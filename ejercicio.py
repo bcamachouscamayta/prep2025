1ae67ac7560eb1f1289649c01b102499cf93cf6c
bcamachouscamayta <evabri2008cue@gmail.com>
2025-10-29 17:09:15 -0300

    se hizo el atacar y curar

---
+++ ejercicio.py
@@ -0,0 +1,84 @@
+
+
+
+
+#esciribir una funcion que reciba un archivo e intente leer los datos
+#del siguiente enemigo si8 no hay mas enemigos debe devolver none
+#si hay un enemigo  devuelbve una lista de 4 valores : nombre, vida
+#daño,probabilidad de daño golpe ,menos el nombre de los otros
+#valores debe ser insts
+
+
+import random
+
+
+def leerEnemigo(archivo):
+   nombre = archivo.readline().strip()
+   if nombre == "":
+      return None
+   vida = int(archivo.readline())
+   daño = int(archivo.readline())
+   probAtaque = int(archivo.readline())
+
+   return [nombre, vida , daño ,probAtaque]
+
+
+def calcularAtaque(prob, fuerza, vida):
+
+  ataque = random.randint (1, 100)
+  if ataque <= prob:
+      print("El ataque impactó")
+      return vida-fuerza
+  else:
+       print("El ataque falló")
+       return vida
+
+
+a = open ("enemigos.txt", "r")
+
+
+enemigo = leerEnemigo(a)
+
+
+vida = 30
+prob = 80
+fuerza = 3
+
+
+while enemigo != None and vida > 0:
+   print(f"vida: {vida}")
+   # Enemigo: Esqueleto, vida: 12
+   print(f"Enemigo:{ enemigo[0] }, vida: {enemigo[1]}")
+
+
+   decicion = input("que vas a hacer: 1) atacar o 2) curarte?: ")
+   while decicion not in ["1", "2"]:
+       print("selecione una opcion correcta")
+       decicion = input("que vas a hacer: 1) atacar o 2) curarte?: ")
+
+
+   if decicion == "1":
+       print(f"atacaste al enemigo!")
+       enemigo[1] = calcularAtaque(prob, fuerza, enemigo[1])
+      
+   if decicion == "2":
+       print(f"te curaste ahora tienes {vida + 5} de vida!")
+       vida = vida + 5
+
+
+   if enemigo[1] <= 0:
+       print(f"derrotaste a {enemigo[0]}")
+       enemigo = leerEnemigo(a)
+       if enemigo != None:
+           print(f"aparecio otro enemigo!{enemigo[0]}")
+   else:
+       print(f"{enemigo[0]} ataca!")
+       vida = calcularAtaque(enemigo[3], enemigo[2],vida)
+
+
+if vida <= 0:
+  print("perdiste")
+else:
+  print("ganaste")
+
+a.close()
