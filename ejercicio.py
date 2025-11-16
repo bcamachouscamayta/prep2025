ee7c87d4e6f0487bbde258fa2e808e40e8de78f6
bcamachouscamayta <evabri2008cue@gmail.com>
2025-11-16 20:22:11 -0300

    de hizo la lista de items falta el final

--- ejercicio.py
+++ ejercicio.py
@@ -1,43 +1,47 @@
 import random
 
 def leerItems():
+
     items = []
-    try: 
-        with open("items.txt", "r") as f:
-            while True:
-                nombre = f.readline().strip()
-                if not nombre:
-                    break
-                tipo = f.readline().strip()
-                cantidad = int(f.readline().strip())
-                items.append([nombre, tipo, cantidad])
+    try:
+        f = open("items.txt", "r", encoding = "utf-8")
     except FileNotFoundError:
-        print("No se encontró el archivo items.txt. No tendrás items.")
+        print("Advertencia: No se encontró 'items.txt'. Jugarás sin items.")
+        return []
+    while True:
+        nombre = f.readline().strip()
+        if nombre == "":
+            break
+        curacion = int(f.readline().strip())
+        items.append([nombre, curacion])
+    f.close()
+    print(f"Items cargados: {[item[0] for item in items]}")
     return items
+
+
+def usarItem(item, items, vida):
    
-def usarItem(item, items, vida, fuerza, vidaMax):
-    if item[1] == "Curar":
-        print(f"Usaste el item {item[0]}, que cura {item[2]} puntos.")
-        vida += item[2]
-        if vida > vidaMax:
-            vida = vidaMax
-            print(f"Como superaste el  limite de vida, quedaste con {vida}")
-        else:
-            print(f"Quedaste con {vida}")
-    elif item[1] == "Atacar":
-        print(f"Usaste el item {item[0]}, que aumenta tu fuerza en {item[2]}.")
-        fuerza += item[2]
-    items.remove(item)
-    return vida, fuerza
+    nombre_item = item[0] #nombre pocision 0
+    curacion_item = item[1] #curacion pocision 1
+
+    print(f"Usaste {nombre_item}, que cura {curacion_item} puntos de vida")
+    vida += curacion_item
+   
+    print(f"Quedaste con {vida} HP.")
+       
+    items.remove(item) # usamos el item
+    return vida
+
 
 def leerEnemigo(archivo):
+
    nombre = archivo.readline().strip()
-   if nombre == "":
+   if not nombre :
       return None
    vida = int(archivo.readline())
    daño = int(archivo.readline())
    probAtaque = int(archivo.readline())
-
+   
    return [nombre, vida , daño ,probAtaque]
 
 
@@ -47,59 +51,93 @@ def calcularAtaque(prob, fuerza, vida):
   if ataque <= prob:
       print("El ataque impactó")
       return vida-fuerza
-
   else:
        print("El ataque falló")
        return vida
 
-a = open ("enemigos.txt", "r")
 
-enemigo = leerEnemigo(a)
+while True:
 
-vida = 30
-prob = 80
-fuerza = 3
-probPotenciada = 50
-fuerzaPotenciada = 7
+    a = open ("enemigos.txt", "r")
 
-while enemigo != None and vida > 0:
-   print(f"vida: {vida}")
-   # Enemigo: Esqueleto, vida: 12
-   print(f"Enemigo:{ enemigo[0] }, vida: {enemigo[1]}")
+    enemigo = leerEnemigo(a)
+    items = leerItems()
 
-
-   decicion = input("que vas a hacer: 1) atacar , 2) ataque potenciado o 3) curarte?: ")
-   while decicion not in ["1", "2", "3"]:
-       print("selecione una opcion correcta")
-       decicion = input("que vas a hacer: 1) atacar , 2) ataque potenciado o 3) cuararte")
-
-   if decicion == "1":
-       print(f"atacaste al enemigo!")
-       enemigo[1] = calcularAtaque(probPotenciada, fuerzaPotenciada, enemigo[1])
-      
-   if decicion == "2":
-    print(f"lanzaste un ataque potenciado!")
-    enemigo[1] = calcularAtaque(probPotenciada, fuerzaPotenciada, enemigo[1])
-
-   if decicion == "3":
-       print(f"te curaste ahora tienes {vida + 5} de vida!")
-       vida = vida + 5
-      
-
-   if enemigo[1] <= 0:
-       print(f"derrotaste a {enemigo[0]}")
-       enemigo = leerEnemigo(a)
-       if enemigo != None:
-           print(f"aparecio otro enemigo!{enemigo[0]}")
-   else:
-       print(f"{enemigo[0]} ataca!")
-       vida = calcularAtaque(enemigo[3], enemigo[2],vida)
+    vida = 30
+    prob = 80
+    fuerza = 3
+    probPotenciada = 50
+    fuerzaPotenciada = 7
 
 
-if vida <= 0:
-  print("perdiste")
-else:
-  print("ganaste")
+    while enemigo != None and vida > 0:
+        print(f"vida: {vida}")
+        # Enemigo: Esqueleto, vida: 12
+        print(f"Enemigo:{ enemigo[0] }, vida: {enemigo[1]}")
 
-a.close()
 
+        decicion = input("que vas a hacer: 1) atacar ,2) ataque potenciado ,3) curarte? o 4)usar un item : ")
+        while decicion not in ["1", "2", "3", "4"]:
+            print("selecione una opcion correcta")
+            decicion = input("que vas a hacer: 1) atacar , 2) ataque potenciado ,3) cuararte o 4)usar un item : ")
+
+        turno_cancelado = False
+        if decicion == "1":
+            print(f"atacaste al enemigo!")
+            enemigo[1] = calcularAtaque(prob, fuerza, enemigo[1])
+           
+        elif decicion == "2":
+                print(f"lanzaste un ataque potenciado!")
+                enemigo[1] = calcularAtaque(probPotenciada, fuerzaPotenciada, enemigo[1])
+
+        elif decicion == "3":
+                print(f"te curaste ahora tienes {vida + 5} de vida!")
+                vida = vida + 5
+           
+        elif decicion == "4":
+            turno_cancelado = True
+           
+            if not items:
+                 print("¡No tienes items para usar!")
+            else:
+                 print("Elige un item (0 para cancelar):")
+                 for i, item in enumerate(items):
+                     print(f" {i+1}) {item[0]}")
+               
+                 while True: # Bucle para elegir el item
+                     try:
+                         item_elegido = int(input("Tu elección: "))
+                         if 0 < item_elegido <= len(items):
+                            # Elige un item válido
+                             item_a_usar = items[item_elegido - 1]
+                             vida = usarItem(item_a_usar, items, vida)
+                             break # Rompe el bucle de elegir item
+                         elif item_elegido == 0:
+                            # Cancela la selección de item
+                             print("Cancelaste el uso de items.")
+                             turno_cancelado = False
+                             break # Rompe el bucle de elegir item
+                         else:
+                             print(f"Error: Elige un número entre 0 y {len(items)}")
+                     except ValueError:
+                         print("Error: Ingresa solo números.")
+
+        if not turno_cancelado:
+            if enemigo[1] <= 0:
+                print(f"derrotaste a {enemigo[0]}")
+                enemigo = leerEnemigo(a)
+                if enemigo != None:
+                    print(f"aparecio otro enemigo!{enemigo[0]}")
+            else:
+                print(f"{enemigo[0]} ataca!")
+                vida = calcularAtaque(enemigo[3], enemigo[2],vida)
+
+
+        else:
+          if vida <= 0:
+            print("perdiste")
+          else:
+            print("ganaste")
+          a.close()
+          break
+    
